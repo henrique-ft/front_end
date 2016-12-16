eb6574464d4a0bec03eeed48af0bf2eaa948a515
hriqueft <hriqueft@yahoo.com.br>
2016-12-16 23:17:39 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+        <link rel="stylesheet" type="text/css" href="css/icons.css">
+        <style type="text/css">
+            
+            body{
+                padding-top: 90px;
+            }
+            
+            .first{
+                border-bottom: 3px solid #4f65ea;
+            }
+
+            
+            .second{
+                border-bottom: 3px solid #ec575a;
+            }
+
+            
+            .third{
+                border-bottom: 3px solid #1da212;
+            }
+
+            .titulo-home{
+            }
+
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <!-- top bar -->
+        
+        <div class="top-bar">
+            
+            <ul>
+                <li><a href="#" class="top-bar-first active">Sobre</a></li>
+                <li><a href="#">Trabalhos</a></li>
+                <li><a href="#">osadasdai</a></li>
+            </ul>    
+            
+        </div>
+        
+        <h1 class="titulo-home">Henrique F. <small>Web Designer</small></h1>
+        
+        <!-- row -->       
+         
+        <div class="row">
+            
+            <div class="five col">
+                
+                <h4 class="second">Formação</h4>
+ 
+ 
+<p>  <code>Code</code>   oLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt euismod lorem, quis lacinia mi rutrum euismod. Pellentesque et pretium sapien, sit amet sodales sapien. In at erat orci. Praesent consectetur, urna eu iaculis interdum, est ante elementum erat, ac lobortis dolor orci vitae urna. Quisque pellentesque varius interdum. Fusce dignissim, ante a tincidunt lacinia, quam ante condimentum nisl, vitae ultrices sapien erat at lacus. Phasellus blandit suscipit ligula. Suspendisse iaculis metus eu mattis mattis. In massa erat, scelerisque sit amet accumsan et, imperdiet vitae purus. Nam varius commodo interdum. Cras elementum consectetur consequat. Vestibulum accumsan mi vel commodo porttitor. Phasellus dictum sollicitudin ante, sit amet egestas sem hendrerit at.
+                
+                Quisque sed metus et libero pellentesque tempor. Morbi finibus blandit mi, nec commodo felis pretium quis. Donec at turpis non sem suscipit bibendum. Sed nec risus mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus lacinia, massa non fringilla blandit, ipsum turpis sagittis massa, a maximus dolor est id ligula. Nullam mattis gravida massa in egestas. Vestibulum sed quam eget dui commodo rutrum at vel nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut aliquam enim neque, quis venenatis lectus cursus commodo. Aliquam sagittis mi eget ante egestas, sit amet tristique sapien euismod. Mauris ligula leo, ornare fermentum sem sed, lacinia fringilla neque. Nunc et nisi facilisis, blandit risus sit amet, porta velit. Phasellus faucibus augue ut tortor volutpat pretium. Maecenas ultricies ultricies facilisis.
+ </p>
+ 
+                <pre>
+                    <code>
+.hey{
+    color: black;
+    content: 'this is a block of code';
+}
+                    </code>
+                </pre>                
+   Quisque sed metus et libero pellentesque tempor. Morbi finibus blandit mi.
+                
+            </div>   
+            
+            <div class="five col">
+                
+                <h4 class="third">Trabalhos <i class="fa fa-cube" aria-hidden="true"></i></h4>
+                <strong>Bolded</strong>
+<em>Italicized</em>
+<a>Colored</a>
+<u>Underlined</u> Nulla luctus augue vel lectus dignissim, vitae finibus felis bibendum. Morbi in fermentum felis. Sed aliquet nunc a maximus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut mauris id nulla bibendum facilisis sed non ligula. Donec non sagittis lectus. Ut eu lacinia lorem. In in est volutpat ante placerat efficitur vel sit amet lorem. Praesent id nisi id enim lacinia elementum.
+                
+                Vestibulum iaculis quis justo a porttitor. In pulvinar pellentesque ante. Duis molestie iaculis nibh, non
+                               
+                                oLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt euismod lorem, quis lacinia mi rutrum euismod. Pellentesque et pretium sapien, sit amet sodales sapien. In at erat orci. Praesent consectetur, urna eu iaculis interdum, est ante elementum erat, ac lobortis dolor orci vitae urna. Quisque pellentesque varius interdum. Fusce dignissim, ante a tincidunt lacinia, quam ante condimentum nisl, vitae ultrices sapien erat at lacus. Phasellus blandit suscipit ligula. Suspendisse iaculis metus eu mattis mattis. In massa erat, scelerisque sit amet accumsan et, imperdiet vitae purus. Nam varius commodo interdum. Cras elementum consectetur consequat. Vestibulum accumsan mi vel commodo porttitor. Phasellus dictum sollicitudin ante, sit amet egestas sem hendrerit at.
+                
+                Quisque sed metus et libero pellentesque tempor. Morbi finibus blandit mi, nec commodo felis pretium quis. Donec at turpis non sem suscipit bibendum. Sed nec risus mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus lacinia, massa non fringilla blandit, ipsum turpis sagittis massa, a maximus dolor est id ligula. Nullam mattis gravida massa in egestas. Vestibulum sed quam eget dui commodo rutrum at vel nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut aliquam enim neque, quis venenatis lectus cursus commodo. Aliquam sagittis mi eget ante egestas, sit amet tristique sapien euismod. Mauris ligula leo, ornare fermentum sem sed, lacinia fringilla neque. Nunc et nisi facilisis, blandit risus sit amet, porta velit. Phasellus faucibus augue ut tortor volutpat pretium. Maecenas ultricies ultricies facilisis.
+                
+                Nulla luctus augue vel lectus dignissim, vitae finibus felis bibendum. Morbi in fermentum felis. Sed aliquet nunc a maximus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut mauris id nulla bibendum facilisis sed non ligula. Donec non sagittis lectus. Ut eu lacinia lorem. In in est volutpat ante placerat efficitur vel sit amet lorem. Praesent id nisi id enim lacinia elementum.
+                
+                Vestibulum iaculis quis justo a porttitor. In pulvinar pellentesque ante. Duis molestie iaculis nibh, non
+            
+            </div>  
+
+            <div class="two col">
+                
+                <h4 >Itens</h4>
+                
+                <p class="title-divider">Title1</p>       
+                <ul>
+                    
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <ul>
+                       <li>Lorem</li>
+                       <li>Lorem</li>
+                       <li>Lorem</li>
+                    </ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    
+                </ul>
+                
+                <p class="title-divider">Title2</p>
+                <ul>
+                    
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <ul>
+                       <li>Lorem</li>
+                       <li>Lorem</li>
+                       <li>Lorem</li>
+                    </ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                    
+                </ul>
+            </div>   
+            
+        </div>
+     
+        <!-- row -->
+        
+        <div class="row">
+            
+            <div class="twelve col">
+                
+                <h4 class="u-s-margin-top title-divider">Form</h4> 
+                <div class="callout sucess">* Lorem ipsum dolor site a met lo rein</div>  
+                <form>
+                    <fieldset>
+                        <div class="row">
+                            <div class="six col">
+                                <label for="exampleEmailInput">Your email</label>
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput"/>                               
+                            </div>
+                            <div class="six col">
+                                  <label for="exampleEmailInput">Your email</label>
+                                <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput"/>                               
+                            </div>                   
+                        </div>
+                        <div class="row">
+                            <div class="twelve col">                 
+                                <label for="exampleRecipientInput">Reason for contacting</label>
+                                  <select class="u-full-width" id="exampleRecipientInput">
+                                    <option value="Option 1">Questions</option>
+                                    <option value="Option 2">Admiration</option>
+                                    <option value="Option 3">Can I get your number?</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="twelve col">                    
+                                  <label for="exampleMessage">Message</label>
+                                  <textarea class="u-full-width" placeholder="Hi Dave …" id="exampleMessage"></textarea>
+                            </div>
+                        </div>
+                         <div class="row">
+                            <div class="twelve col">           
+                                 <label><input id="confirm" type="checkbox" value="oi"> <span class="label-body"> Send a copy to yourself</span></label>
+                            </div>
+                        </div>                   
+                        <input class="button-primary" type="submit" value="Submit">
+                    </fieldset>
+                </form>
+                
+            </div>
+            
+        </div>
+
+        <!-- row -->
+        
+        <div class="row">
+            
+            <div class="twelve col">
+                
+                <h4 class="u-s-margin-top title-divider">Table</h4>
+                
+                <table class="u-full-width">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Age</th>
+                      <th>Sex</th>
+                      <th>Location</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Dave Gamache</td>
+                      <td>26</td>
+                      <td>Male</td>
+                      <td>San Francisco</td>
+                    </tr>
+                    <tr>
+                      <td>Dwayne Johnson</td>
+                      <td>42</td>
+                      <td>Male</td>
+                      <td>Hayward</td>
+                    </tr>
+                  </tbody>
+                </table>
+            </div>
+        </div>
+        
+        <!-- row -->
+        
+        <div class="row">
+            
+            <div class="twelve col">
+                
+                <h4 class="u-s-margin-top title-divider">Buttons</h4>
+                
+                <div class="row">
+                   <!-- Standard buttons -->
+                    <a class="button" href="#">Anchor button</a>
+                    <button>Button element</button>
+                    <input type="submit" value="submit input">
+                    <input type="button" value="button input">
+                <div>
+                    <br>
+                <div class="row">
+                    <!-- Primary buttons -->
+                    <a class="button button-primary" href="#">Anchor button</a>
+                    <button class="button-primary">Button element</button>
+                    <input class="button-primary" type="submit" value="submit input">
+                    <input class="button-primary" type="button" value="button input">
+                <div>                
+                
+            </div>
+        </div>
+
+        <!-- row -->
+        
+        <div class="row">
+            
+            <div class="twelve col">
+                
+                <h4 class="u-s-margin-top title-divider">Panel</h4>
+
+                <div class="panel">
+                    
+                    <h4>Hey, i´m a panel</h4>
+                    <p>
+                          oLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt euismod lorem, quis lacinia mi rutrum euismod. Pellentesque et pretium sapien, sit amet sodales sapien. In at erat orci. Praesent consectetur, urna eu iaculis interdum, est ante elementum erat, ac lobortis dolor orci vitae urna. Quisque pellentesque varius interdum. Fusce dignissim, ante a tincidunt lacinia, quam ante condimentum nisl, vitae ultrices sapien erat at lacus. Phasellus blandit suscipit ligula. Suspendisse iaculis metus eu mattis mattis. In massa erat, scelerisque sit amet accumsan et, imperdiet vitae purus. Nam varius commodo interdum. Cras elementum consectetur consequat. Vestibulum accumsan mi vel commodo porttitor. Phasellus dictum sollicitudin ante, sit amet egestas sem hendrerit at.
+                
+                        Quisque sed metus et libero pellentesque tempor. Morbi finibus blandit mi, nec c            
+                        
+                    </p>
+                </div>   
+                 
+            </div>
+        </div>
+
+        <!-- Callout -->
+        
+        <div class="row">
+            
+            <div class="twelve col">
+                
+                <h4 class="u-s-margin-top title-divider">Callouts</h4>
+
+                <div class="callout">callout</div> 
+                
+                <div class="callout sucess">callout sucess</div>     
+                
+                <div class="callout error">callout error</div> 
+                
+                <div class="callout warning">callout warning</div>  
+                 
+            </div>
+        </div>
+     
+    </body>
+    
+    
+</html>
